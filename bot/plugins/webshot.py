aa15abb3d3ed5c335b05dc7318075bfa1cad775b
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2024-05-15 12:10:46 +0200

    update the plugins

--- bot/plugins/webshot.py
+++ bot/plugins/webshot.py
@@ -12,26 +12,22 @@ class WebshotPlugin(Plugin):
         return "WebShot"
 
     def get_spec(self) -> [Dict]:
-        return [
-            {
-                "name": "screenshot_website",
-                "description": "Show screenshot/image of a website from a given url or domain name.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "url": {
-                            "type": "string",
-                            "description": "Website url or domain name. Correctly formatted url is required. Example: https://www.google.com",
-                        }
-                    },
-                    "required": ["url"],
+        return [{
+            "name": "screenshot_website",
+            "description": "Show screenshot/image of a website from a given url or domain name.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "url": {"type": "string",
+                            "description": "Website url or domain name. Correctly formatted url is required. Example: https://www.google.com"}
                 },
-            }
-        ]
+                "required": ["url"],
+            },
+        }]
 
     def generate_random_string(self, length):
         characters = string.ascii_letters + string.digits
-        return "".join(random.choice(characters) for _ in range(length))
+        return ''.join(random.choice(characters) for _ in range(length))
 
     async def execute(self, function_name, helper, **kwargs) -> Dict:
         try:
@@ -47,23 +43,21 @@ class WebshotPlugin(Plugin):
                 if not os.path.exists("uploads/webshot"):
                     os.makedirs("uploads/webshot")
 
-                image_file_path = os.path.join(
-                    "uploads/webshot", f"{self.generate_random_string(15)}.png"
-                )
+                image_file_path = os.path.join("uploads/webshot", f"{self.generate_random_string(15)}.png")
                 with open(image_file_path, "wb") as f:
                     f.write(response.content)
 
                 return {
-                    "direct_result": {
-                        "kind": "photo",
-                        "format": "path",
-                        "value": image_file_path,
+                    'direct_result': {
+                        'kind': 'photo',
+                        'format': 'path',
+                        'value': image_file_path
                     }
                 }
             else:
-                return {"result": "Unable to screenshot website"}
+                return {'result': 'Unable to screenshot website'}
         except:
-            if "image_file_path" in locals():
+            if 'image_file_path' in locals():
                 os.remove(image_file_path)
 
-            return {"result": "Unable to screenshot website"}
+            return {'result': 'Unable to screenshot website'}
